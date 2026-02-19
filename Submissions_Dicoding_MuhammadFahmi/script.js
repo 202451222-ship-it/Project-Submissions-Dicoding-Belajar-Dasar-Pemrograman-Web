a5294a399cec07ffac6056c399877d2ff666c75a
Fahmi Fahrobi <202451222@std.umk.ac.id>
2026-02-19 09:56:18 +0700

    Update script.js

--- Submissions_Dicoding_MuhammadFahmi/script.js
+++ Submissions_Dicoding_MuhammadFahmi/script.js
@@ -1,1 +1,13 @@
+const hamburgerButton = document.querySelector('#hamburger');
+const drawerMenu = document.querySelector('#drawer');
 
+hamburgerButton.addEventListener('click', function(event) {
+    drawerMenu.classList.toggle('open');
+    event.stopPropagation();
+});
+
+document.body.addEventListener('click', function(event) {
+    if (!hamburgerButton.contains(event.target) && !drawerMenu.contains(event.target)) {
+        drawerMenu.classList.remove('open');
+    }
+});
